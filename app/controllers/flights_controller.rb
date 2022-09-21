37ef6b9e49aa276266cbaa046eaa6c7a7c5f59be
Kasper Høglund <kasper@rubynor.no>
2022-09-21 15:45:13 +0200

    Let user only select dates with avaible flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,8 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @airports = Airport.all.map {|airport| [airport.airport_code, airport.id]}
     @passengers = (1..4).map {|n| [n, n]}
-    dates = @flights.order(:departure_time).map {|flight| flight.departure_time.strftime('%m.%d.%Y')}
-    @dates = dates.uniq
+    @dates = @flights.order(:departure_time).map{|flight| [Time.at(flight.departure_time).strftime('%d.%m.%Y'), Time.at(flight.departure_time).midnight.to_i]}.uniq
+
+    @results =  Flight.all
   end
 end
