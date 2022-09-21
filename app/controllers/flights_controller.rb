570e9db8c66faf52716aced5fbda6ba790b13869
Kasper Høglund <kasper@rubynor.no>
2022-09-21 13:12:01 +0200

    flights#index added

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,9 @@
 class FlightsController < ApplicationController
+  def index
+    @flights = Flight.all
+    @airports = Airport.all.map {|airport| [airport.airport_code, airport.id]}
+    @passengers = (1..4).map {|n| [n, n]}
+    dates = @flights.order(:departure_time).map {|flight| flight.departure_time.strftime('%m.%d.%Y')}
+    @dates = dates.uniq
+  end
 end
