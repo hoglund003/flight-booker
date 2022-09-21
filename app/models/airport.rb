fefb67b131e97a3840e5c4397fceda91efe1007c
Kasper Høglund <kasper@rubynor.no>
2022-09-21 10:41:46 +0200

    Flight model + accosiations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,3 +1,8 @@
 class Airport < ApplicationRecord
   validates :airport_code, uniqueness: true, presence: true
+
+  has_many :departing_flights, foreign_key: "departure_airport_id", class_name: "Flight"
+  has_many :arriving_flights, foreign_key: "arrival_airport_id", class_name: "Flight"
+
+  # has_many :departing_flights
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,24 @@ codes = ["ABR",
   "AUS"]
 
 Airport.delete_all
+# Flight.delete_all
 
 30.times do
   Airport.create(airport_code: codes.sample)
+end
+
+100.times do 
+  def rand_time(from=7.days.from_now, to=14.days.from_now)
+    Time.at(rand(from.to_f..to.to_f))
+  end
+  departure = Airport.pluck(:id).sample
+  arrival = Airport.pluck(:id).sample
+  datetime = rand_time
+  duration = rand_time(1.hours, 5.hours)
+  Flight.create!(
+    departure_airport_id: departure,
+    arrival_airport_id: arrival,
+    departure_time: datetime,
+    flight_duration: duration
+  )
 end
