6bb7bf3375db69a9b5f15bcef6e6fc6948f2b142
Kasper Høglund <kasper@rubynor.no>
2022-09-22 11:01:39 +0200

    accosiations added

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, foreign_key: "departure_airport_id", class_name: "Airport"
   belongs_to :arrival_airport, foreign_key: "arrival_airport_id", class_name: "Airport"
+
+  has_many :bookings
+  has_many :passengers, through: :bookings
 end
