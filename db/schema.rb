a635abed7b7dd81dd80d8a9102e91dead3a7662f
Kasper Høglund <kasper@rubynor.no>
2022-09-22 10:12:52 +0200

    booking model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_150330) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_081031) do
   create_table "airports", force: :cascade do |t|
     t.string "airport_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,6 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_150330) do
     t.integer "arrival_airport_id"
     t.integer "departure_time"
     t.integer "flight_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
