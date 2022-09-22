ef4183b3e753d697e82ab20a84232ee11446bf63
Kasper Høglund <kasper@rubynor.no>
2022-09-22 15:26:14 +0200

    bookings#create added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,17 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     params[:number_of_passengers].to_i.times { @booking.passengers.build }
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [
+      :id, :name, :email
+    ])
+  end
 end
